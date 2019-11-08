63a992e749e13b87d5c9132ddbc361a981456cfc
lucasmelo <lucas.melo@geofusion.com.br>
2019-11-08 18:54:13 -0300

    gitignore e alteracao para execucao de teste

--- src/test/java/cadastro/CadastraCorrentistaRunner.java
+++ src/test/java/cadastro/CadastraCorrentistaRunner.java
@@ -1,9 +1,11 @@
 package cadastro;
 
+import com.intuit.karate.KarateOptions;
 import com.intuit.karate.junit4.Karate;
 import org.junit.runner.RunWith;
 
 @RunWith(Karate.class)
+@KarateOptions(features = "classpath:cadastro/cadastra_correntista.feature")
 public class CadastraCorrentistaRunner {
 
 }
